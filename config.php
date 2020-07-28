7a3bf0c6bbe64bd9d89f24acc41f917b34713ec7
Sebasthian1998 <46504242+Sebasthian1998@users.noreply.github.com>
2020-07-28 13:18:50 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 $XVWA_WEBROOT = "";
-$host = "localhost";
-$dbname = 'xvwa';
-$user = "root";
-$pass = "";
+$host = "tviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$dbname = 'miqkye20qpl1ugt7';
+$user = "p5iwgamah3l3kqd6";
+$pass = "q8rkcefwmhq4umh9";
 $conn = new mysqli($host,$user,$pass,$dbname);
 $conn1 = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 $conn1->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
